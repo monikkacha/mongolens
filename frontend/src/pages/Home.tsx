936f72ea00c991a1474831b81153e562583d26a5
priyakacha <priyakacha16@gmail.com>
2026-01-03 16:49:36 +0500

    find Error type and slove the error

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -15,7 +15,6 @@ import { useNavigate } from "react-router-dom";
 import { testConnection } from "@/services/apis/connection";
 import Logo from "@/assets/logo.png";
 
-
 interface SavedConnection {
   id: string;
   name: string;
@@ -42,8 +41,12 @@ const HomePage: React.FC = () => {
       if (res.data.success) {
         navigate("/connections", { state: { connectionString } });
       }
-    } catch (error: any) {
-      alert(error.response?.data?.message || "Connection failed");
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message || "Connection failed");
+      } else {
+        alert("Connection failed");
+      }
     } finally {
       setLoading(false);
     }
@@ -55,14 +58,6 @@ const HomePage: React.FC = () => {
 
   return (
     <div className="relative min-h-screen bg-[#080e0e] text-white overflow-hidden">
-
-      {/* ambient glows */}
-      {/* <div className="absolute inset-0 pointer-events-none">
-        <div className="absolute top-24 left-16 w-52 h-52 rounded-full bg-[#7FC8F8]/20 blur-3xl" />
-        <div className="absolute bottom-24 right-24 w-72 h-72 rounded-full bg-[#4A5D73]/20 blur-3xl" />
-      </div> */}
-
-      {/* saved connections */}
       <div className="absolute right-6 top-10 z-20">
         <Dialog>
           <DialogTrigger asChild>
@@ -83,14 +78,16 @@ const HomePage: React.FC = () => {
             </Button>
           </DialogTrigger>
 
-          <DialogContent className="
+          <DialogContent
+            className="
         bg-[#2C3646]/95
         border border-[#4A5D73]
         backdrop-blur-xl
         text-white
         shadow-2xl
         rounded-2xl
-      ">
+      "
+          >
             <DialogHeader>
               <DialogTitle className="text-lg tracking-wide">
                 Saved Connections
@@ -139,12 +136,7 @@ const HomePage: React.FC = () => {
                         <Button
                           size="sm"
                           onClick={() => handleDelete(conn.id)}
-                          className="
-                        bg-[#E5533D]/90
-                        hover:bg-[#e72b0e]
-                        text-white
-                        transition
-                      "
+                          className="bg-[#E5533D]/90 hover:bg-[#e72b0e] text-white transition"
                         >
                           Delete
                         </Button>
@@ -158,15 +150,15 @@ const HomePage: React.FC = () => {
         </Dialog>
       </div>
 
-
-      {/* main split layout */}
       <div className="relative min-h-screen bg-[#080e0e]">
         <div className="flex items-center justify-center px-6">
           <Card className="absolute flex justify-center top-[282px] left-[580px] w-[777px] h-[313px] bg-[#080e0e] border-0 ">
-
             <CardHeader className="space-y-1">
               <CardTitle className="text-3xl font-semibold tracking-wide text-white">
-                <img src={Logo} className="w-[90px] h-[90px] top-282 left-623 rounded-full mx-auto" />
+                <img
+                  src={Logo}
+                  className="w-[90px] h-[90px] top-282 left-623 rounded-full mx-auto"
+                />
               </CardTitle>
               <p className="text-[24px] text-center p-3 letter-space-0 h-100% text-[#F6F6F6] font-Roboto ">
                 A new way to browse MongoDB data
@@ -175,7 +167,8 @@ const HomePage: React.FC = () => {
 
             <CardContent className="flex items-center gap-4 p-0">
               <Input
-                placeholder="mongodb://localhost:27017 "
+                placeholder="
+                mongodb://localhost:27017 "
                 value={connectionString}
                 onChange={(e) => setConnectionString(e.target.value)}
                 className="w-[600px] h-[60px] bg-[#1B1E1F] rounded-sm pl-6 text-9xl text-white border-0 font-light placeholder:text-white placeholder:font-light placeholder:text-[24px]"
@@ -184,19 +177,14 @@ const HomePage: React.FC = () => {
               <Button
                 onClick={handleConnect}
                 disabled={loading || !connectionString}
-                className="bg-[#61BAB9] hover:bg-[#61BAB9] font-Roboto font-medium text-[#ffffff] rounded-sm text-[18px] w-[160px] h-[60px] top-[535px] left-[867px]">
+                className="bg-[#61BAB9] hover:bg-[#61BAB9] font-Roboto font-medium text-[#ffffff] rounded-sm text-[18px] w-[160px] h-[60px] top-[535px] left-[867px]"
+              >
                 {loading ? "Connecting..." : "Connect"}
               </Button>
             </CardContent>
           </Card>
         </div>
       </div>
-
-      {/* <footer className="px-8 py-28 flex text-center justify-center">
-        <div className="mt-6 text-sm font-normal text-red-500 bg-amber-100">
-          <p>Hi welcome to mongolenss</p>
-        </div>
-      </footer> */}
     </div>
   );
 };
